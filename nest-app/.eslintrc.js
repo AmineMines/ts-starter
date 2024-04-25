ee41eecac7ced27963a7dbebad9a2874438b3cae
pjmartin <pierre-jean.martin@gojob.com>
2024-04-25 11:57:11 +0200

    feat(eslint) gojob-eslint in nest-app

--- nest-app/.eslintrc.js
+++ nest-app/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    '@gojob/eslint-config/typescript',
   ],
   root: true,
   env: {
